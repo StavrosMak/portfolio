bc0fb2b71c6b6627abcd61ecb180cfd78eb6a37c
StV <stv@StV.Workstation>
2023-09-21 22:33:39 +0300

    fixed Links

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -27,11 +27,11 @@ export default function Navbar() {
         className={`leftSideNav ${menuOpen ? 'active' : ''}`}
       >
         <ul className='navLinks'>
-          <li><Link to='/#' onClick={toggleMenu} smooth>Home</Link></li>
-          <li><Link to='/#about' onClick={toggleMenu} smooth>About</Link></li>
-          <li><Link to='/#skills' onClick={toggleMenu} smooth>Skills</Link></li>
-          <li><Link to='/#projects' onClick={toggleMenu} smooth>Projects</Link></li>
-          <li><Link to='/#contact' onClick={toggleMenu} smooth>Contact</Link></li>
+          <li><Link to='/portfolio/#' onClick={toggleMenu} smooth>Home</Link></li>
+          <li><Link to='/portfolio/#about' onClick={toggleMenu} smooth>About</Link></li>
+          <li><Link to='/portfolio/#skills' onClick={toggleMenu} smooth>Skills</Link></li>
+          <li><Link to='/portfolio/#projects' onClick={toggleMenu} smooth>Projects</Link></li>
+          <li><Link to='/portfolio/#contact' onClick={toggleMenu} smooth>Contact</Link></li>
         </ul>
       </div>
     </nav>
